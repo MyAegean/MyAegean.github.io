197a71de648fd7536d500b1e40eb6a9939b7bea8
My.Aegean.gr  Team <myaegean@aegean.gr>
2017-03-30 14:19:09 +0300

    status app
    status pages

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm776921448-b7fb9ff1bb5b28997f07781f',
+	'm776921451-0015c3bd61757df94d21e48c',
+	'm776921454-de33837cfcef0a2e2e83c726',
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
