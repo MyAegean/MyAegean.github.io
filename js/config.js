4da2b13df8c74da220929d1cc9dc692468122826
My.Aegean.gr Team <myaegean@aegean.gr>
2017-04-03 00:00:38 +0300

    additional triggers/monitors

--- js/config.js
+++ js/config.js
@@ -3,6 +3,8 @@ var __apiKeys = [
 	'm776921448-b7fb9ff1bb5b28997f07781f',
 	'm776921451-0015c3bd61757df94d21e48c',
 	'm776921454-de33837cfcef0a2e2e83c726',
+	'm778873836-4d37b929ce8e982cb1c8eea4',
+	'm778873835-221cd6110d51efe054345101',
 ];
 
 // refresh interval (in seconds)
